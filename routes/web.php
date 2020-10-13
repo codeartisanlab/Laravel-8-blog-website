4121200a13902b487110bc0bdf53f2cb065297fa
unknown <codeartisanlab2607@gmail.com>
2020-10-13 21:47:39 +0530

    part-3 Request and Response Cycle

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function(){
+//     return view('welcome');
+// });
+
+Route::get('/',[HomeController::class,'home']);
