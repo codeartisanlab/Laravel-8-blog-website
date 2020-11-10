d5c5fea229740db63e607e573ed58d4f8815f73a
unknown <codeartisanlab2607@gmail.com>
2020-11-10 23:23:22 +0530

    part-8-admin-manage-category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+        $data=Category::all();
+        return view('backend.category.index',['data'=>$data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('backend.category.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required'
+        ]);
+
+        if($request->hasFile('cat_image')){
+            $image=$request->file('cat_image');
+            $reImage=time().'.'.$image->getClientOriginalExtension();
+            $dest=public_path('/imgs');
+            $image->move($dest,$reImage);
+        }
+
+        $category=new Category;
+        $category->title=$request->title;
+        $category->detail=$request->detail;
+        $category->image=$reImage;
+        $category->save();
+
+        return redirect('admin/category/create')->with('success','Data has been added');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data=Category::find($id);
+        return view('backend.category.update',['data'=>$data]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title'=>'required'
+        ]);
+
+        if($request->hasFile('cat_image')){
+            $image=$request->file('cat_image');
+            $reImage=time().'.'.$image->getClientOriginalExtension();
+            $dest=public_path('/imgs');
+            $image->move($dest,$reImage);
+        }else{
+            $reImage=$request->cat_image;
+        }
+
+        $category=Category::find($id);
+        $category->title=$request->title;
+        $category->detail=$request->detail;
+        $category->image=$reImage;
+        $category->save();
+
+        return redirect('admin/category/'.$id.'/edit')->with('success','Data has been added');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Category::where('id',$id)->delete();
+        return redirect('admin/category');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,4 +24,6 @@ Route::get('/',[HomeController::class,'home']);
 
 Route::get('/admin/login',[AdminController::class,'login']);
 Route::post('/admin/login',[AdminController::class,'submit_login']);
-Route::get('/admin/dashboard',[AdminController::class,'dashboard']);
+
+Route::get('admin/category/{id}/delete',[CategoryController::class,'destroy']);
+Route::resource('admin/category',CategoryController::class);
