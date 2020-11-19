3cac6a41b1b8aa78842bf8ea220533c3b1994259
unknown <codeartisanlab2607@gmail.com>
2020-11-19 07:40:02 +0530

    part-13-blog-home-page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Post;
 class HomeController extends Controller
 {
     function home(){
-        return view('home');
+    	$posts=Post::all();
+        return view('home',['posts'=>$posts]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,89 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
+    <title>Home Page</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="{{asset('lib')}}/bs4/bootstrap.min.css" />
+    <!-- Jquery -->
+    <script type="text/javascript" src="{{asset('lib')}}/jquery-3.5.1.min.js"></script>
+    <!-- BS4 Js -->
+    <script type="text/javascript" src="{{asset('lib')}}/bs4/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <h3>Hello World</h3>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    	<div class="container">
+		  <a class="navbar-brand" href="#">CodeArtisanLab</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNav">
+		    <ul class="navbar-nav ml-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Categories</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">My Account</a>
+		      </li>
+		    </ul>
+		  </div>
+		</div>
+	</nav>
+	<!-- Get latest posts -->
+	<main class="container mt-4">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="row"> 
+					@if(count($posts)>0)
+						@foreach($posts as $post)
+						<div class="col-md-4">
+							<div class="card">
+							  <a href="{{url('detail/'.$post->id)}}"><img src="{{asset('imgs/thumb/'.$post->thumb)}}" class="card-img-top" alt="{{$post->title}}" /></a>
+							  <div class="card-body">
+							    <h5 class="card-title"><a href="{{url('detail/'.$post->id)}}">{{$post->title}}</a></h5>
+							  </div>
+							</div>
+						</div>
+						@endforeach
+					@else
+					<p class="alert alert-danger">No Post Found</p>
+					@endif
+				</div>
+			</div>
+			<!-- Right SIdebar -->
+			<div class="col-md-4">
+				<!-- Search -->
+				<div class="card mb-4">
+					<h5 class="card-header">Search</h5>
+					<div class="card-body">
+						<div class="input-group">
+						  <input type="text" class="form-control" />
+						  <div class="input-group-append">
+						    <button class="btn btn-dark" type="button" id="button-addon2">Search</button>
+						  </div>
+						</div>
+					</div>
+				</div>
+				<!-- Recent Posts -->
+				<div class="card mb-4">
+					<h5 class="card-header">Recent Posts</h5>
+					<div class="list-group list-group-flush">
+						<a href="#" class="list-group-item">Post 1</a>
+						<a href="#" class="list-group-item">Post 2</a>
+					</div>
+				</div>
+				<!-- Popular Posts -->
+				<div class="card mb-4">
+					<h5 class="card-header">Popular Posts</h5>
+					<div class="list-group list-group-flush">
+						<a href="#" class="list-group-item">Post 1</a>
+						<a href="#" class="list-group-item">Post 2</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 </body>
 </html>
